3f18c74eae06a8d31257734e99fbb20c98fe5830
alessiorubicini <alessiorubicini05@gmail.com>
2025-03-28 16:06:38 +0100

    Fixed bug in components

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -54,8 +55,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation }) => {
+          const Icon = orientation === 'left' ? ChevronLeftIcon : ChevronRightIcon;
+          return <Icon className='h-4 w-4' />;
+        },
       }}
       {...props}
     />
