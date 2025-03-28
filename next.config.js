6cad824924dd4f9fd0abce758751f50e395b0a9c
alessiorubicini <alessiorubicini05@gmail.com>
2025-03-28 16:53:50 +0100

    Fixed next config

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '',
+};
+
+module.exports = nextConfig;
